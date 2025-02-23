1806d9cdc88e2cd72c9362e26fa54334270200c8
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-23 23:45:11 +0200

    Task 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+# Print all possible different combinations of two digits.
+# The two digits must be different.
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9:
+            print("{:d}{:d}, ".format(i, j), end="")
+        else:
+            print("{:d}{:d}".format(i, j))
