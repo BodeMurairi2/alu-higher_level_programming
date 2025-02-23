b64b62509d8dc9dcc4cf803dc24b7cca11b790a2
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-24 00:40:31 +0200

    Task 11

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# This python script return the result of a number raised to a power
+
+
+def pow(a, b):
+    '''Return the result of a number raised to a power'''
+    return a ** b
