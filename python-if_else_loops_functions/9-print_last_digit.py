38d3ac035b447bdcf14cadebd18a594efd9a8fff
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-24 00:30:47 +0200

    Last digit task 9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/bin/usr/python3
+def print_last_digit(number):
+    '''Prints the last digit of a number'''
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
