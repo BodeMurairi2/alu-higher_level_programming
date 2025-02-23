b99cfafaba90bb131a07ae2b3d8ab5a1195cab21
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-24 00:09:03 +0200

    Task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+# This script checks for uppercase characters in a string
+
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 65 and ord(i) <= 90:
+            return True
+    return False
