2ce74370e322b2164c462a2f5e95dad1012af29a
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-25 18:39:51 +0200

    Task 100

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# This script the ASCII Alphabet in reverse order, alternating lower and uppercase letter
+
+
+for i in range(122, 96, -1):
+    print("{:c}".format(i if (i % 2 == 0) else (i - 32)), end='')
