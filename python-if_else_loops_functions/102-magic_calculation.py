cac0151338ac6ebef6b3e9c9ac47630ae64e59b8
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-25 19:04:39 +0200

    Project 2 completed

---
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Magic calculation
+
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif b > c:
+        return a + b
+    else:
+        return a * b - c
