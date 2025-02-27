10040b84b180fd75c67648872ae366c96dad24a3
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-27 11:06:11 +0200

    Task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-# This script import add function
-# from file add_o.py
+# This script imports the add function from add_0.py
 
 from add_0 import add
 
-a = 1
-b = 2
 
-print(f"{a} + {b} = {add(a, b)}")
+def main():
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
+
+
+if __name__ == "__main__":
+    main()
