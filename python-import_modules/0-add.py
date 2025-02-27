cd5ed06d8d9e341cff681f45ad553a58d554fdc7
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-27 12:35:48 +0200

    Task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 # This script imports the add function from add_0.py
 
-from add_0 import add
 
-
-def main():
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-
-if __name__ == "__main__":
-    main()
