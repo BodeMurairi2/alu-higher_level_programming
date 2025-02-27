0276562075be5d8791805a9c0b809a510551db24
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-27 12:58:58 +0200

    Task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,8 +2,10 @@
 # This script imports the add function from add_0.py
 
 
+from add_0 import add
+
+
 if __name__ == "__main__":
-    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b))) # Use add(a, b) only once
+    print("{} + {} = {}".format(a, b, add(a, b)))   # Use add(a, b) only once
