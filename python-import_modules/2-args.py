4dfcbfda539dc162ef60b99dc6d6763ae78c064a
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-28 18:44:41 +0200

    Task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments")
+    else:
+        print("{:d} argument".format(len(sys.argv) - 1), end="")
+        if len(sys.argv) > 1:
+            print("s", end="")
+        print(":")
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
