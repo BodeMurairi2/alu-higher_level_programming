ec212e16be68816878d1cc389870fafb780faa92
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-28 18:46:05 +0200

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         print("{:d} argument".format(len(sys.argv) - 1), end="")
         if len(sys.argv) > 1:
             print("s", end="")
-        print(":")
+            print(":")
         for i in range(1, len(sys.argv)):
             print("{:d}: {:s}".format(i, sys.argv[i]))
