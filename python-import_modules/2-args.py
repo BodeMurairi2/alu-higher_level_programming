7538dd27c4b9d83f16194ee233159f83c9a83047
BodeMurairi2 <b.murairi@alustudent.com>
2025-02-28 19:20:27 +0200

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 
 
-import sys
-
-
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print("0 arguments")
+    import sys
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print("{:d} argument".format(len(sys.argv) - 1), end="")
-        if len(sys.argv) > 1:
-            print("s", end="")
-        print(":")
-        for i in range(1, len(sys.argv)):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
