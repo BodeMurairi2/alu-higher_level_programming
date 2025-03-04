c5b4cb05783c04ecad3e3e33833604a8387ec8f7
BodeMurairi2 <b.murairi@alustudent.com>
2025-03-04 13:57:47 +0200

    Task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,5 +4,6 @@ def element_at(my_list, idx):
     """Retrieve an element from a list at a given index"""
     if idx < 0 or idx >= len(my_list):
         return None
-    return "Element at index {:d} is {}".format(idx,
-                                                my_list[idx])
+    return "Element at index {:d} is {:d}".format(
+        idx, my_list[idx]
+        )
