d25bd32f0995c1a6115c163d34aac96bc3c3df2c
BodeMurairi2 <b.murairi@alustudent.com>
2025-03-10 16:34:46 +0200

    Max number

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max_numb = 0
+    for i in range(len(my_list)):
+        if my_list[i] > max_numb:
+            max_numb = my_list[i]
+    return "{:d}".format(max_numb)
