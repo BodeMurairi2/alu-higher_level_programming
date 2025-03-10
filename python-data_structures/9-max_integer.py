7dc220852e3b149276237739a42dda7d3ec5240b
BodeMurairi2 <b.murairi@alustudent.com>
2025-03-10 16:37:27 +0200

    Task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     max_numb = 0
-    for i in my_list:
+    for i in my_list.sort():
         if i > max_numb:
             max_numb = i
     return "{:d}".format(max_numb)
