78234ca40e1f499f2c62820efbf13d93f1e53378
BodeMurairi2 <b.murairi@alustudent.com>
2025-03-10 16:02:37 +0200

    Task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,8 +2,6 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure both tuples have at least two elements, filling missing values with 0
     a1, a2 = (tuple_a + (0, 0))[:2]
     b1, b2 = (tuple_b + (0, 0))[:2]
-    
     return (a1 + b1, a2 + b2)
