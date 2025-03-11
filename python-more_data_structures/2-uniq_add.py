5fd72c13f83730564a692e6a5d1e13fb8eaa6dad
BodeMurairi2 <b.murairi@alustudent.com>
2025-03-11 11:15:04 +0200

    Task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@
 
 def uniq_add(my_list=[]):
     sum = 0
-    for i in range(len(my_list)):
-        if my_list[i] != my_list[i-1]:
-            sum += my_list[i]
+    new_list = list(set(my_list))
+    for i in new_list:
+        sum += i
     return sum
