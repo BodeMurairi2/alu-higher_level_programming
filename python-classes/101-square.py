4f182c56b5aacac8498b0919e258bea9b7f212b8
BodeMurairi2 <b.murairi@alustudent.com>
2025-03-16 04:55:47 +0200

    Task 101

---
+++ python-classes/101-square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+"""
+Square module
+"""
+
+
+class Square:
+    """
+    A class that defines a square.
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initializes the Square instance.
+        Args:
+            size (int): The size of the square (default is 0).
+            position (tuple): The position of the square (default is (0, 0)).
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter for position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter for position with validation."""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) and num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square using the '#' character, considering position."""
+        if self.__size == 0:
+            print()
+            return
+        print("\n" * self.__position[1], end="")
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """Defines the string representation of the Square instance."""
+        if self.__size == 0:
+            return ""
+        lines = [
+            " " * self.__position[0] + "#" * self.__size
+            for _ in range(self.__size)
+            ]
+        return "\n" * self.__position[1] + "\n".join(lines)
