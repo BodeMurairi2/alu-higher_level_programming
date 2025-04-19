e0260b953af1c667623f62b81971380712f18615
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-19 11:38:42 +0200

    Task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,20 +5,29 @@ This module contains a class BaseGeometry
 
 
 class BaseGeometry:
-    """This is class defines a base geometry"""
+    """
+    This is class defines a base geometry
+    """
+
     def __init__(self, width, height):
-        """Initialize width and height"""
+        """
+        Initialize width and height
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-    
+
     def area(self):
-        """This method raises an exception"""
+        """
+        This method raises an exception
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value as integer"""
+        """
+        Validates value as integer
+        """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
