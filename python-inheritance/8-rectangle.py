867d8d83f7fea1b9d69f9acfaea0421bdebad269
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-19 11:41:50 +0200

    Task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,34 +1,59 @@
 #!/usr/bin/python3
 """
-This module contains a class BaseGeometry
+This module contains a class BaseGeometry and its derived class Rectangle
 """
 
 
 class BaseGeometry:
     """
-    This is class defines a base geometry
+    This class defines a base geometry for other geometric shapes to inherit.
     """
-
-    def __init__(self, width, height):
-        """
-        Initialize width and height
-        """
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
 
     def area(self):
         """
-        This method raises an exception
+        Method that raises an exception when called. Should be implemented by
+        derived classes to calculate the area.
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
-        Validates value as integer
+        Validates if the value is a positive integer.
+
+        Args:
+            name (str): The name of the attribute.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
         """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    This class defines a rectangle and inherits from BaseGeometry.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes the rectangle with a validated width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Validates that both width and height are positive integers.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
