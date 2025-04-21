cd905a18c2bf0e598dbf92a43980f69789b74870
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-21 12:52:04 +0200

    Task 01

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -16,6 +16,3 @@ def write_file(filename="", text=""):
     """
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(text)
-        file.write('\n')
-        file.close()
-    return
