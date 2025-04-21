832141c0eae7e49cba0372d4e408f55c3eae90c0
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-21 12:04:36 +0200

    Task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,38 +1,18 @@
 #!/usr/bin/python3
-"""Define a class Square that inherits from Rectangle."""
-
-
-class Rectangle():
-    """this class represents a rectangle using BaseGeometry"""
-
-    def __init__(self, width, height):
-        """Intialize a new rectangle"""
-
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """returns the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """returns the print() and str() representation of a Rectangle"""
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
-        return string
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """this class represents a square using Rectangle"""
+    """Represent a square"""
+
     def __init__(self, size):
-        """Initialize a new square """
-        if isinstance(size, int) and size > 0:
-            self.__size = size
-        else:
-            raise TypeError("size must be a positive integer")
+        """Initialize a new square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
     def area(self):
-        """returns the area of the square"""
-        return self.__size * self.__size
+        """Return the area of the square"""
+        return self.__size ** 2
