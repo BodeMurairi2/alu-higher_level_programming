1fa40da7649761349e19bce4bef06777966125f8
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-21 14:04:04 +0200

    Task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a class Student with 
+"""Defines a class Student with
 a method to return its JSON representation."""
 
 
@@ -19,11 +19,14 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
-            If attrs is a list of strings, represents only those attributes
-            included in the list.
-            Args:
-                attrs (list): (Optional) The attributes to represent.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
         """
-    if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
-        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-    return self.__dict__
+        if isinstance(attrs, list) and all(
+            isinstance(ele, str) for ele in attrs
+        ):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+
+        return self.__dict__
