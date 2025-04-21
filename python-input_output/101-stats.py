ca489c99ee6636153c62d31072a3a674cb535fc1
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-21 14:18:44 +0200

    Task 101

---
+++ python-input_output/101-stats.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_status():
+    """Prints the status of the request every 10 lines."""
+    counter = 0
+    size = 0
+    status_codes = {
+        "200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0
+    }
+
+    try:
+        for line in sys.stdin:
+            parts = line.split()
+            if len(parts) < 2:
+                continue
+
+            try:
+                size += int(parts[-1])
+                code = parts[-2]
+                if code in status_codes:
+                    status_codes[code] += 1
+            except (ValueError, IndexError):
+                continue
+
+            counter += 1
+            if counter == 10:
+                print("File size: {}".format(size))
+                for key in sorted(status_codes):
+                    if status_codes[key]:
+                        print("{}: {}".format(key, status_codes[key]))
+                counter = 0
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("File size: {}".format(size))
+        for key in sorted(status_codes):
+            if status_codes[key]:
+                print("{}: {}".format(key, status_codes[key]))
+
+
+if __name__ == "__main__":
+    print_status()
