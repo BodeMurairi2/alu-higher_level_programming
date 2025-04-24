d8094068cbb14cecbbdbc5c8dc15bcecc2d2fa79
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-24 18:46:27 +0200

    Task 01

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,6 @@
+-- This script create a database called hbtn_0c_0 in my MySQL server
+-- If the database already exists, my script will not failed.
+
+-- This command will crate a new database named `hbtn_0c_0` 
+-- if it does not already exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
