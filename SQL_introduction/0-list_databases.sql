ad3c2e77e01bb4ce673be664423bdeb1465874b0
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-24 18:42:25 +0200

    Task 0

---
+++ SQL_introduction/0-list_databases.sql
@@ -0,0 +1,5 @@
+-- This SQL script lists all databases in the current MySQL server instance.
+-- It uses the `SHOW DATABASES` command to retrieve the names of all databases.
+
+-- This command will display the names of all databases available in the MySQL server.
+SHOW DATABASES;
