685673b69cfaf3decfc5a145f0eb25d48bbc9660
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-24 19:03:09 +0200

    Task 05

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,6 +2,4 @@
 -- from the database hbtn_0c_0, without using DESCRIBE or EXPLAIN
 
 -- -- These commands achieve the task
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table' AND TABLE_SCHEMA = 'hbtn_0c_0';
+SHOW CREATE TABLE first_table;
