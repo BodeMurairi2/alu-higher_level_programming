cb43dfe0a0d42c46127bbab8b5ce2afb10676410
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-24 18:49:25 +0200

    Task 02

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- This script deletes a database called hbtn_0c_0 in my MySQL server
+-- -- If the database does not exist, my script will not failed.
+
+-- This command deletes a database named `hbtn_0c_0` if exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
