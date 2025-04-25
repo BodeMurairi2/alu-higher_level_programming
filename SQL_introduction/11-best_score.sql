c75ad6d7151d408efa1f9618ea4138093326813e
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-25 18:42:46 +0200

    Task 11

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -2,4 +2,4 @@
 -- It will display the score and the name
 
 -- This command achieves the task
-SELECT score, name FROM second_table WHERE score > 10;
+SELECT score, name FROM second_table WHERE score > 10 ORDER BY score DESC;
