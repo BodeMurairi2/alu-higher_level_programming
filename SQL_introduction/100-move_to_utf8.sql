505e65c1171b6d78f572ee2cb0c137229bf8bb17
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-25 19:04:49 +0200

    TAsk 100

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- Convert the database to UTF8 (utf8mb4 with collation utf8mb4_unicode_ci)
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Convert the table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the 'name' field to UTF8
+ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
