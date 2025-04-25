335eb677bdb8c9d4af11b5f496bb6c903513d15c
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-25 19:16:58 +0200

    Task 101

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,6 @@
--- This script imports another table in "temperatures.sql" in the database
--- Then calculates the average temperature in Fahrenheit
-
--- This command achieves the task
-SELECT city, AVG(value) AS average_temperature FROM temperatures GROUP BY city ORDER BY average_temperature DESC;
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
