2f6513604625b3d4f70e49d3871d625f2830e56f
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-30 17:39:37 +0200

    Task 01

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,6 +5,7 @@ header variable found in the response header.
 import urllib.request
 import sys
 
+
 def fetch_header(url):
     """Fetches a URL and displays the value of the X-Request-Id header."""
     with urllib.request.urlopen(url) as response:
