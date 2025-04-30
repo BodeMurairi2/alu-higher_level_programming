f1743b43033481ea7b82d6f9937ebba69371b4be
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-30 17:28:13 +0200

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,14 +7,10 @@ def fetch_status(url='https://alu-intranet.hbtn.io/status'):
     """Fetches the status of a URL and prints the response."""
     with urllib.request.urlopen(url) as response:
         body = response.read()
-        print("Body response: ")
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode('utf-8')))
-        print("\t- status code: {}".format(response.getcode()))
-        print("\t- headers: {}".format(response.getheaders()))
-        print("\t- URL: {}".format(response.geturl()))
-        print("\t- status:".format(response.status))
 
 
 if __name__ == "__main__":
