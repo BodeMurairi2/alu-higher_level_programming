e56437c4b8ec2d40cbb8d0c52a8669381526396a
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-30 17:39:07 +0200

    Task 01

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module fetches a URL and displays the value of the X-Request-Id
+header variable found in the response header.
+"""
+import urllib.request
+import sys
+
+def fetch_header(url):
+    """Fetches a URL and displays the value of the X-Request-Id header."""
+    with urllib.request.urlopen(url) as response:
+        # Get the value of the X-Request-Id header
+        x_request_id = dict(response.headers).get('X-Request-Id')
+        return x_request_id
+
+
+if __name__ == "__main__":
+    print(fetch_header(url))
