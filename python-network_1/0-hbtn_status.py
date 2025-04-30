73913eeb16fab554579d142caadc3507f2fabf19
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-30 17:25:25 +0200

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,13 +2,20 @@
 """This module defines a function that fetches the status of a URL."""
 import urllib.request
 
-with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("Body response: ")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
-    print("\t- status code: {}".format(response.getcode()))
-    print("\t- headers: {}".format(response.getheaders()))
-    print("\t- URL: {}".format(response.geturl()))
-    print("\t- status:".format(response.status))
+
+def fetch_status(url='https://alu-intranet.hbtn.io/status'):
+    """Fetches the status of a URL and prints the response."""
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response: ")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- status code: {}".format(response.getcode()))
+        print("\t- headers: {}".format(response.getheaders()))
+        print("\t- URL: {}".format(response.geturl()))
+        print("\t- status:".format(response.status))
+
+
+if __name__ == "__main__":
+    fetch_status()
