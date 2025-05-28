066e957cc7bdd85b59e9dd639712b8c8f4780988
BodeMurairi2 <b.murairi@alustudent.com>
2025-05-28 12:41:07 +0200

    Task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,14 +2,14 @@
 
 const x = parseInt(process.argv[2]);
 
-function factorial(n) {
-    if (isNaN(n)) {
-        return 1;
-    }
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(x));
