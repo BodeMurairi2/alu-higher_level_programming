a5f7b7a7b2ba75654258579232f1e518551ce056
BodeMurairi2 <b.murairi@alustudent.com>
2025-05-29 10:47:38 +0200

    First tutorial js

---
+++ learn_js/hello-world/hello.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+/* print an alert */
+alert("Hello World");
---
+++ learn_js/session.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+var name = "bode";
+console.log(name);
+let age = 20;
+let isHuman = true;
+let students = ["bode", "ibrahim", "chadrack"];
+var name = "15";
+console.log(name);
+
+
+var name = "dirac";
+console.log(name);
