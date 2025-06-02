3820bffe5e9ec3d0a5b8bdb8280d955a50bd414e
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-03 01:01:32 +0200

    finobacci challenge

---
+++ learn_js/javascript_challenge/finobucci.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+function finobucci (number) {
+  const sequence = [];
+  let a = 0;
+  let b = 1;
+  for (let i = 0; i < number; i++) {
+    sequence.push(a);
+    const temp = a;
+    a = b;
+    b = temp + b;
+  }
+  let sum_even_numbers = 0;
+  for (const nums of sequence) {
+    if (nums < 4000000 && nums % 2 === 0) {
+      sum_even_numbers = sum_even_numbers + nums;
+    }
+  }
+  console.log(`Sums of all sequence number less than 4000000 and even in the finobucci sequence is: ${sum_even_numbers}`);
+}
+
+finobucci(25);
+
