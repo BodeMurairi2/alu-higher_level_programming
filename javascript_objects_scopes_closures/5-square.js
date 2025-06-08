6f08104013a7de79ff3e39612a09a0b34dc36bc6
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-08 07:12:53 +0200

    task 05

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// square class that defines a square and inherit from Rectangle
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+    constructor(size){
+        super(size, size)
+    }}
+
+module.exports = Square;
