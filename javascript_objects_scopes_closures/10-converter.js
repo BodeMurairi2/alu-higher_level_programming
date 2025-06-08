6e88160754f1d2948d11bf5e40b5cb2617ffdc7d
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-08 19:57:04 +0200

    Task 10

---
+++ javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// This script convert a number from base 10 to another base
+
+console.log((+process.argv[2]).toString(+process.argv[3]));
---
+++ javascript_objects_scopes_closures/converter_try.js
@@ -0,0 +1,50 @@
+#!/usr/bin/node
+
+function converterBase1 (number, base) {
+  if (number === 0) {
+    const numberInNewBase = '0';
+    console.log(numberInNewBase);
+  } else {
+    const listRest = [];
+    while (number / base !== 0) {
+      listRest.push(number % base);
+      number = Math.floor(number / base);
+    }
+    const rest = [];
+    for (let i = listRest.length - 1; i >= 0; i--) {
+      rest.push(listRest[i]);
+    }
+    let numberInNewBase = '';
+    for (const rests of rest) {
+      numberInNewBase += rests;
+    }
+    return numberInNewBase;
+  }
+}
+
+function converterBase2 (number, base) {
+  if (number === 0) {
+    console.log('0');
+  } else {
+    const listRest = [];
+    while (number > 0) {
+      listRest.push(number % base);
+      number = Math.floor(number / base);
+    }
+
+    const rest = [];
+    for (let i = listRest.length - 1; i >= 0; i--) {
+      rest.push(listRest[i]);
+    }
+
+    let numberInNewBase = '';
+    for (const digit of rest) {
+      numberInNewBase += digit;
+    }
+
+    return numberInNewBase;
+  }
+}
+
+console.log(converterBase1(3, 2));
+console.log(converterBase2(3, 2));
