319ba6198bba17392924237525b652e67c83419d
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-08 06:52:40 +0200

    Task 04

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const Rectangle = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let printf = '';
+    for (let x = 0; x < this.height; x++) {
+      for (let y = 0; y < this.width; y++) {
+        printf = printf + 'X';
+      }
+      console.log(printf);
+      printf = '';
+    }
+  }
+
+  rotate () {
+    const c = this.width;
+    this.width = this.height;
+    this.height = c;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
+
+module.exports = Rectangle;
