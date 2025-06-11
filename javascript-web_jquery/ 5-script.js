949627a3ba8880f4c1b9ff4fc1181ed74034cf41
BodeMurairi2 <b.murairi@alustudent.com>
2025-06-11 23:00:59 +0200

    Task 06

--- javascript-web_jquery/ 5-script.js
+++
@@ -1,7 +0,0 @@
-// add li element to a list when add_item is clicked
-
-$(document).ready(function () {
-    $("#add_item").click(function () {
-        $(".my_list").append("<li>Item</li>");
-    });
-});
---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+// update the header to new header
+
+$(document).ready(function () {
+  $('#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
+});
